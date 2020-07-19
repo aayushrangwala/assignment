448943f6b61a105d288c9ecd4aad27334ade19dc
Ayush Rangwala <aayush.rangwala@trilio.io>
2020-07-19 14:41:32 +0530

    TASK-6: optimized and refactored the cipher functions

--- internal/constants.go
+++ internal/constants.go
@@ -7,6 +7,12 @@ const (
 	// Shift is the number of shift offset to be used to encrypt the string in encode endpoint
 	Shift = 32
 
+	// CeaserCipherEncode will be used to trigger the encoding operation for ceaser cipher using the helper function
+	CeaserCipherEncode = 1
+
+	// CeaserCipherDecode will be used to trigger the decoding operation for ceaser cipher using the helper function
+	CeaserCipherDecode = -1
+
 	// AlphabetsLength is the length of alphabets
 	AlphabetsLength = 26
 )
