21782d4392b75c3ebcae7fd69fb4bf6476839ec5
Ayush Rangwala <aayush.rangwala@trilio.io>
2020-07-19 12:46:39 +0530

    TASK-5: Added a new HTTP endpoint http://127.0.0.1:3333/encode/xxx

--- constants.go
+++ constants.go
@@ -1,4 +1,12 @@
 package main
 
-// DefaultServicePort is the port on which the service is going to Listen and serve
-const DefaultServicePort = 3333
+const (
+	// DefaultServicePort is the port on which the service is going to Listen and serve
+	DefaultServicePort = 3333
+
+	// EncryptionShift is the number of shift offset to be used to encrypt the string in encode endpoint
+	EncryptionShift = 32
+
+	// AlphabetsLength is the length of alphabets
+	AlphabetsLength = 26
+)
--- endpoints_test.go
+++ endpoints_test.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func TestSimplePath(t *testing.T) {
@@ -14,9 +16,9 @@ func TestSimplePath(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		randomPath := generateRandomPath()
 
-		req, err := http.NewRequest("GET", "/"+randomPath, nil)
+		req, err := http.NewRequest(http.MethodGet, "/"+randomPath, nil)
 		if err != nil {
-			t.Fatal(err)
+			t.Error(err)
 		}
 
 		// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
@@ -25,7 +27,6 @@ func TestSimplePath(t *testing.T) {
 
 		// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
 		// directly and pass in our Request and ResponseRecorder.
-
 		handler.ServeHTTP(rr, req)
 
 		t.Logf("response for %s request path is: %s", req.URL.String(), rr.Body.String())
@@ -39,9 +40,9 @@ func TestSimplePath(t *testing.T) {
 
 func TestHealth(t *testing.T) {
 	for i := 0; i < 10; i++ {
-		req, err := http.NewRequest("GET", "/healthz", nil)
+		req, err := http.NewRequest(http.MethodGet, "/healthz", nil)
 		if err != nil {
-			t.Fatal(err)
+			t.Error(err)
 		}
 
 		// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
@@ -50,13 +51,76 @@ func TestHealth(t *testing.T) {
 
 		// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
 		// directly and pass in our Request and ResponseRecorder.
-
 		handler.ServeHTTP(rr, req)
 
 		t.Logf("response for %s request path is: %s", req.URL.String(), rr.Body.String())
 		// Check the status code is what we expect.
 		if status := rr.Code; status != http.StatusOK {
 			t.Errorf("endpoint failed: Status codes: [Expected: %v], [Returned: %v]", status, http.StatusOK)
+		}
+	}
+}
+
+func TestCeaserCipherEncode(t *testing.T) {
+	var testCases = []struct {
+		in, out string
+		shift   int
+	}{
+		{"if-he-had-anything-confidential", "ol-nk-ngj-gteznotm-iutlojktzogr", 32},
+		{"to-say", "zu-yge", 32},
+		{"by-so-changing-the", "if-zv-johunpun-aol", 7},
+		{"order-of-the", "xamna-xo-cqn", 9},
+		{"letters of the", "exmmxkl hy max", 19},
+		{"alphabet, that", "ximexybq, qexq", 23},
+		{"not-a-word-could", "yze-l-hzco-nzfwo", 37},
+		{"be made out.", "mp xlop zfe.", 11},
+	}
+
+	for _, tc := range testCases {
+		out := ceaserCipherEncode(tc.in, tc.shift)
+		t.Logf("Ceaser cipher, Input: [%s], Shift: [%d], Expected: [%s], out: [%s]", tc.in, tc.shift, tc.out, out)
+
+		if out != tc.out {
+			t.Errorf("[FAILED]. expected: %v, returned: %v ", tc.out, out)
+		}
+	}
+}
+
+func TestEncode(t *testing.T) {
+	var testCases = []struct {
+		in, out string
+	}{
+		{"If-he-had-anything-confidential", "Ol-nk-ngj-gteznotm-iutlojktzogr"},
+		{"to-say", "zu-yge"},
+		{"by-so-changing-the", "he-yu-ingtmotm-znk"},
+		{"order-of-the", "uxjkx-ul-znk"},
+		{"letters-of-the", "rkzzkxy-ul-znk"},
+		{"alphabet,-that", "grvnghkz,-zngz"},
+		{"not-a-word-could", "tuz-g-cuxj-iuarj"},
+		{"be-made-out.", "hk-sgjk-uaz."},
+	}
+
+	for _, tc := range testCases {
+		req, err := http.NewRequest(http.MethodGet, "/encode", nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		req = mux.SetURLVars(req, map[string]string{"input": tc.in})
+
+		// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(Encode)
+
+		// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+		// directly and pass in our Request and ResponseRecorder.
+		handler.ServeHTTP(rr, req)
+
+		t.Logf("response for %s request path is: %s", req.URL.String(), rr.Body.String())
+		// Check the status code is what we expect.
+		if status := rr.Code; status != http.StatusOK && rr.Body.String() != tc.out {
+			t.Errorf("endpoint failed: Status codes: [Expected: %v], [Returned: %v], Cipher: [Expected %v], [Returned %v]",
+				status, http.StatusOK, tc.out, rr.Body.String())
 		}
 	}
 }
